05b417cc4736f41b424edb286d2a80eee635a126
Jade Kim <csia19030@gmail.com>
2024-12-06 15:02:48 +0900

    feat: 계산기 버튼 클릭시 화면 출력

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hi");
+const numBtns = document.querySelectorAll('.num');
+const operBtns = document.querySelectorAll('.oper');
+const res = document.querySelector('.result');
+
+let equation = "";
+
+numBtns.forEach(function(value){
+    value.addEventListener("click", function(event){
+        equation += event.target.innerHTML;
+        console.log(equation);
+        res.innerHTML = equation;
+    })
+})
+
+operBtns.forEach(function(value){
+    value.addEventListener("click", function(event){
+        equation += event.target.innerHTML;
+        console.log(equation);
+        res.innerHTML = equation;
+    })
+})
+
