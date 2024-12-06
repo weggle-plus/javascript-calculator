2fa59746db1d45da9f41eab984eca83667be72e6
rkdcodus <rcd7275@naver.com>
2024-12-06 17:04:44 +0900

    feat: 사칙 연산 기능 구현 중
    *, % 구현 중인데 float 처리 안되고 있음.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const row = document.querySelectorAll(".row");
 const result = document.querySelector(".result");
 const log = document.querySelector(".log");
-const operator = ["<", "%", "x", "-", "+", ".", "="];
-let equation = "";
+const operator = ["%", "x", "-", "+"];
+let equation = "5%55";
 
 row.forEach(function (item) {
   item.addEventListener("click", function (event) {
@@ -22,3 +22,35 @@ row.forEach(function (item) {
     }
   });
 });
+
+function calculate() {
+  // x, % 먼저 찾고 양 옆 숫자를 찾아 계산한다. -> 대체
+  // 순차적으로 계산
+  const number = equation.split(/[%x+-]/).map(Number);
+  const oper = equation.split(/[0-9.]/).filter((item) => item);
+
+  while (oper.includes("x") || oper.includes("%")) {
+    console.log(number);
+    console.log(oper);
+
+    if (oper.includes("x")) {
+      const index = oper.indexOf("x");
+
+      number[index] = number[index] * number[index + 1];
+      number.splice(index + 1, 1);
+      oper.splice(index, 1);
+    } else if (oper.includes("%")) {
+      const index = oper.indexOf("%");
+
+      number[index] = number[index] / number[index + 1];
+      number.splice(index + 1, 1);
+      oper.splice(index, 1);
+    }
+  }
+  // 5 % 55 * 2 안됨.
+  // +, - 구현 필요
+  console.log(number);
+  console.log(oper);
+}
+
+calculate();
