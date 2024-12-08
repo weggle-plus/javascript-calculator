ec246be252012e3d65afd9744e3ef662d046d5e1
Jade Kim <csia19030@gmail.com>
2024-12-08 14:49:02 +0900

    feat: 곱하기, 나누기 기능 구현

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const row = document.querySelectorAll(".row");
 const result = document.querySelector(".result");
 const log = document.querySelector(".log");
 const operator = ["%", "x", "-", "+"];
-let equation = "5%55";
+let equation = "5%55x2";
 
 row.forEach(function (item) {
   item.addEventListener("click", function (event) {
@@ -26,29 +26,26 @@ row.forEach(function (item) {
 function calculate() {
   // x, % 먼저 찾고 양 옆 숫자를 찾아 계산한다. -> 대체
   // 순차적으로 계산
+
   const number = equation.split(/[%x+-]/).map(Number);
   const oper = equation.split(/[0-9.]/).filter((item) => item);
 
-  while (oper.includes("x") || oper.includes("%")) {
-    console.log(number);
-    console.log(oper);
-
-    if (oper.includes("x")) {
-      const index = oper.indexOf("x");
-
-      number[index] = number[index] * number[index + 1];
-      number.splice(index + 1, 1);
-      oper.splice(index, 1);
-    } else if (oper.includes("%")) {
-      const index = oper.indexOf("%");
-
-      number[index] = number[index] / number[index + 1];
-      number.splice(index + 1, 1);
-      oper.splice(index, 1);
+  let operIdx = 0;
+  while(operIdx < oper.length){
+    if(oper[operIdx] == 'x' || oper[operIdx] == '*'){
+        number[operIdx] = number[operIdx] * number[operIdx + 1];
+        number.splice(operIdx + 1, 1);
+        oper.splice(operIdx, 1);
+        continue;
+    } else if(oper[operIdx] == '%' || oper[operIdx] == '/'){
+        number[operIdx] = number[operIdx] / number[operIdx + 1];
+        number.splice(operIdx + 1, 1);
+        oper.splice(operIdx, 1);
+        continue;
     }
+    operIdx++;
   }
-  // 5 % 55 * 2 안됨.
-  // +, - 구현 필요
+
   console.log(number);
   console.log(oper);
 }
