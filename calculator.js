521439024855b7073c5ddd6779b7e6156aa309d6
hyunsunny3 <hs9473@naver.com>
2024-12-09 17:47:10 +0900

    예외처리 고도화 중

--- calculator.js
+++ calculator.js
@@ -1,78 +1,127 @@
-function addToDisplay(value){
-    document.getElementById("display").value += value;
+let inputDisplay = document.getElementById('display')
+let outputDisplay = document.getElementById('result')
+
+function isExistOutputDisplayValue(){
+    if(outputDisplay.value.length){
+        return true;
+    }
+    return false;
 }
 
-function reset(){
-    document.getElementById("display").value = "";
+// 결과값이 있을 때
+//  ㄴ 연산자만 입력 가능
+//  ㄴ 전체 삭제 가능
+// 연산자 연속입력 방지
+//  ㄴ 입력값 마지막이 연산자일 때 연산자 입력 불가능
+//  ㄴ 아무 입력값 없을 때 연산자 입력 불가능
+
+function addElementToDisplay(element){
+    const operators = ["+", "-", "/", "*"];
+    let inputValue = inputDisplay.value;
+    if (isExistOutputDisplayValue()){
+        if(operators.includes(element)){
+            inputDisplay.value = outputDisplay.value + element;
+            outputDisplay.value = "";
+        }
+        return;
+    }
+    if(operators.includes(element)){
+
+
+        if(inputDisplay.value[inputDisplay.value.length -1]){}
+
+    }
+    inputDisplay.value += element;
 }
 
-function calculate(){
-    const display = document.getElementById("display");
-    const displayValue = display.value;
-    try{
-        const numbers = displayValue.match(/(\d+(\.\d+)?)/g).map(Number);
-        const operators = displayValue.match(/[\+\-\x\/]/g);
-        console.log("displayValue: " + displayValue);
-        console.log("numbers: " + numbers);
-        console.log("operators: " + operators);
-        
-        if (!operators){
-            display.value = numbers[0];
+function resetDisplay(){
+    inputDisplay.value = "";
+    outputDisplay.value = "";
+}
+
+function removeElement(){
+    if (isExistOutputDisplayValue()){
+        return;
+    }
+
+    inputDisplay.value = inputDisplay.value.slice(0,-1);
+}
+
+function getResult(){
+    // console.log(calculatePostfix(infixToPostfix('1+2-3*2+1.2')));
+    const postfix = infixToPostfix(inputDisplay.value);
+    const result = calculatePostfix(postfix);
+    
+    outputDisplay.value = result;
+}
+
+
+function calculate(operand1, operand2, operator) {
+    operand1 = Number(operand1);
+    operand2 = Number(operand2);
+    switch (operator){
+        case '+' : return operand1 + operand2;
+        case '-' : return operand1 - operand2;
+        case '*' : return operand1 * operand2;
+        case '/' : return operand1 / operand2;
+    }
+}
+
+// 후위표기법으로 변환
+function infixToPostfix(infix){
+    const operatorPrecedence = {
+        '+' : 0, 
+        '-' : 0,
+        '*' : 1,
+        '/' : 1
+    };
+    const postfix = [];
+    const operatorStack = [];
+    const elements = infix.match(/\d+(\.\d+)?|[+\-*/]/g);
+
+    elements.forEach(element => {
+        if(isNaN(element)){
+            while(operatorStack.length > 0){
+                const operatorStackPeek = operatorStack[operatorStack.length - 1];
+                if(operatorPrecedence[element] <= operatorPrecedence[operatorStackPeek]){
+                    postfix.push(operatorStack.pop());
+                }else{
+                    break;
+                }
+            }
+            operatorStack.push(element);
             return;
         }
+        postfix.push(element);
+    });
 
-        let multipleAndDivision = (numbers, operators) => {
-            let isMultiple = operators.includes("x");
-            let isDivision = operators.includes("/");
-            let index;
-            if (!isMultiple && !isDivision){
-                return addAndSub(numbers, operators);
-            }
-
-            if (isMultiple){
-                index = operators.indexOf("x");
-            } else {
-                index = operators.indexOf("/");
-            }
-
-            const left = numbers[index];
-            const right = numbers[index + 1];
-            const operator = operators[index];
-            let result;
-            if (operator === "x"){
-                result = left * right;
-            } else {
-                result = left / right;
-            }
-
-            numbers.splice(index, 2, result);
-            operators.splice(index, 1);
-
-            return multipleAndDivision(numbers, operators);
-        }
-
-        let addAndSub = (numbers, operators) => {
-            let result = numbers[0];
-            const calculate = (index) => {
-                if (index >= operators.length){
-                    return result;
-                }
-
-                const operator = operators[index];
-                const number = numbers[index + 1];
-
-                if (operator === "+"){
-                    result += number;
-                } else if (operator === "-"){
-                    result -= number;
-                }
-
-                return calculate(index + 1);
-            }
-            return calculate(0);
-        }
-        display.value = multipleAndDivision(numbers, operators);
-    } catch(error){
-        display.value = "오류";
+    while(operatorStack.length > 0){
+        postfix.push(operatorStack.pop())
     }
+    // console.log(postfix);
+    return postfix;
+}
+// console.log(infixToPostfix('1+2-3*2+1.2'));
+
+
+function calculatePostfix(postfix){
+    const operandStack = [];
+
+    while(postfix.length > 0){
+        const front = postfix[0];
+
+        if(isNaN(front)){
+            const operand2 = operandStack.pop();
+            const operand1 = operandStack.pop();
+
+            const value = calculate(operand1, operand2, front);
+            operandStack.push(value);
+            postfix.shift();
+        }else{
+            operandStack.push(postfix.shift());
+        }
+    }
+    const answer = operandStack.pop();
+    
+    return answer;
 }
--- hsTest.js
+++ hsTest.js
@@ -98,3 +98,84 @@ const test3 = "{[}";
 console.log(`"${test1}"의 유효성:`, isValidParentheses(test1)); // true
 console.log(`"${test2}"의 유효성:`, isValidParentheses(test2)); // false
 console.log(`"${test3}"의 유효성:`, isValidParentheses(test3)); // false
+
+
+
+function addToDisplay(value){
+  document.getElementById("display").value += value;
+}
+
+function reset(){
+  document.getElementById("display").value = "";
+}
+
+function calculate(){
+  const display = document.getElementById("display");
+  const displayValue = display.value;
+  try{
+      const numbers = displayValue.match(/(\d+(\.\d+)?)/g).map(Number);
+      const operators = displayValue.match(/[\+\-\x\/]/g);
+      console.log("displayValue: " + displayValue);
+      console.log("numbers: " + numbers);
+      console.log("operators: " + operators);
+      
+      if (!operators){
+          display.value = numbers[0];
+          return;
+      }
+
+      let multipleAndDivision = (numbers, operators) => {
+          let isMultiple = operators.includes("x");
+          let isDivision = operators.includes("/");
+          let index;
+          if (!isMultiple && !isDivision){
+              return addAndSub(numbers, operators);
+          }
+
+          if (isMultiple){
+              index = operators.indexOf("x");
+          } else {
+              index = operators.indexOf("/");
+          }
+
+          const left = numbers[index];
+          const right = numbers[index + 1];
+          const operator = operators[index];
+          let result;
+          if (operator === "x"){
+              result = left * right;
+          } else {
+              result = left / right;
+          }
+
+          numbers.splice(index, 2, result);
+          operators.splice(index, 1);
+
+          return multipleAndDivision(numbers, operators);
+      }
+
+      let addAndSub = (numbers, operators) => {
+          let result = numbers[0];
+          const calculate = (index) => {
+              if (index >= operators.length){
+                  return result;
+              }
+
+              const operator = operators[index];
+              const number = numbers[index + 1];
+
+              if (operator === "+"){
+                  result += number;
+              } else if (operator === "-"){
+                  result -= number;
+              }
+
+              return calculate(index + 1);
+          }
+          return calculate(0);
+      }
+      display.value = multipleAndDivision(numbers, operators);
+  } catch(error){
+      display.value = "오류";
+  }
+}
