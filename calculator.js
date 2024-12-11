fe14c9a32a24d7b7f0b509a84f6c2d4ede9b8c09
Jini <wlsdl6685@gmail.com>
2024-12-11 12:38:30 +0900

    모듈화 진행중

--- calculator.js
+++ calculator.js
@@ -1,94 +1,9 @@
-let inputDisplay = document.getElementById('display');
-let outputDisplay = document.getElementById('result');
-let buttons = document.querySelectorAll('.buttons button');
 const elementType = {
     NUMBER: 1,
     OPERATOR: 2,
     DECIMALPOINT: 3,
     EQUALS: 4
 };
-
-limitInput(inputDisplay);
-
-buttons.forEach(button => {
-    button.addEventListener('click', function(e){
-        e.preventDefault();
-        handleInput(e);
-    });
-});
-
-inputDisplay.addEventListener('keydown', function(e){
-    e.preventDefault();
-    handleInput(e);
-});
-
-// 입력제한
-function limitInput(input) {
-    const regex = /^[0-9+\-*./]*$/;
-    input.addEventListener('input',function () {
-        if (!regex.test(this.value)) {
-            this.value = this.value.replace(/[^0-9+\-*./]/g, '');
-        }
-    });
-}
-
-function handleInput(e){
-    let value;
-    
-    if(e.type === 'click'){
-        value = e.target.value;
-    }else if(e.type === 'keydown'){
-        const key = e.key;
-        if(getElementType(key)){
-            value = key;
-        }
-
-        if (key === 'Enter' || key === '=') { //Enter > 결과값 출력(=)
-            value = "="
-        }
-        if (key === 'Backspace') {
-            removeElement();
-            return;
-        }
-        if (key === 'Escape') { //ESC > 전체 삭제(AC)
-            resetDisplay();
-            return;
-        }
-    }
-
-    if(value){
-        switch (value) {
-            case '=':
-                getAnswerAndDisplay();
-                break;
-            case 'AC':
-                resetDisplay();
-                break;
-            case '<':
-                removeElement();
-                break;
-            default:
-                addElementToDisplay(value);
-                break;
-        }
-    }
-}
-
-// 버튼 (입력) 이벤트
-function addElementToDisplay(element) {
-    const inputValue = inputDisplay.value;
-    const type = getElementType(element);
-
-    if (type === elementType.OPERATOR) {
-        element = processOperatorElement(inputValue, element);
-    } else if (isExistOutputDisplayValue()) {
-        return;
-    } else if (type === elementType.DECIMALPOINT) {
-        element = processDecimalPointElement(inputValue, element);
-    }
-
-    inputDisplay.value += element;
-}
 
 // 계산기 로직
 function calculate(operand1, operand2, operator) {
@@ -163,13 +78,6 @@ function calculatePostfix(postfix) {
     return new Decimal(answer);
 }
 
-function getAnswerAndDisplay() {
-    let inputValue = 0 + inputDisplay.value;
-    const answer = getAnswer(inputValue);
-
-    outputDisplay.value = answer;
-}
-
 function getAnswer(inputValue) {
     const lastElementType = getLastElementType(inputValue);
 
@@ -181,57 +89,6 @@ function getAnswer(inputValue) {
     const result = calculatePostfix(postfix);
 
     return result;
-}
-
-function resetDisplay() {
-    inputDisplay.value = '';
-    outputDisplay.value = '';
-}
-
-function removeElement() {
-    if (isExistOutputDisplayValue()) {
-        return;
-    }
-
-    inputDisplay.value = inputDisplay.value.slice(0, -1);
-}
-
-// 결과값 있을 때
-function isExistOutputDisplayValue() {
-    if (outputDisplay.value.length) {
-        return true;
-    }
-    return false;
-}
-
-// 연산자 처리
-function processOperatorElement(inputValue, element) {
-    if (isExistOutputDisplayValue()) {
-        inputDisplay.value = outputDisplay.value + element;
-        outputDisplay.value = '';
-        return '';
-    }
-
-    const lastElementType = getLastElementType(inputValue);
-    if (lastElementType === elementType.OPERATOR) {
-        return '';
-    }
-
-    return element;
-}
-
-// 소수점 처리
-function processDecimalPointElement(inputValue, element) {
-    const lastElementType = getLastElementType(inputValue);
-    if (lastElementType === elementType.DECIMALPOINT) {
-        return '';
-    }
-
-    if (lastElementType !== elementType.NUMBER) {
-        return '0' + element;
-    }
-
-    return element;
 }
 
 // 마지막 요소 타입 처리
@@ -266,4 +123,9 @@ function getElementType(element) {
     return undefined;
 }
 
-module.exports = { getAnswer };
+module.exports = {
+    elementType,
+    getAnswer,
+    getLastElementType,
+    getElementType
+}
---
+++ index.js
@@ -0,0 +1,150 @@
+const {
+    elementType,
+    getAnswer,
+    getLastElementType,
+    getElementType,
+} = require('./calculator');
+
+let inputDisplay = document.getElementById('display');
+let outputDisplay = document.getElementById('result');
+let buttons = document.querySelectorAll('.buttons button');
+
+limitInput(inputDisplay);
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+        handleInput(e);
+    });
+});
+
+inputDisplay.addEventListener('keydown', function (e) {
+    e.preventDefault();
+    handleInput(e);
+});
+
+// 입력제한
+function limitInput(input) {
+    const regex = /^[0-9+\-*./]*$/;
+    input.addEventListener('input', function () {
+        if (!regex.test(this.value)) {
+            this.value = this.value.replace(/[^0-9+\-*./]/g, '');
+        }
+    });
+}
+
+function handleInput(e) {
+    let value;
+
+    if (e.type === 'click') {
+        value = e.target.value;
+    } else if (e.type === 'keydown') {
+        const key = e.key;
+        if (getElementType(key)) {
+            value = key;
+        }
+
+        if (key === 'Enter' || key === '=') { //Enter > 결과값 출력(=)
+            value = "="
+        }
+        if (key === 'Backspace') {
+            removeElement();
+            return;
+        }
+        if (key === 'Escape') { //ESC > 전체 삭제(AC)
+            resetDisplay();
+            return;
+        }
+    }
+
+    if (value) {
+        switch (value) {
+            case '=':
+                getAnswerAndDisplay();
+                break;
+            case 'AC':
+                resetDisplay();
+                break;
+            case '<':
+                removeElement();
+                break;
+            default:
+                addElementToDisplay(value);
+                break;
+        }
+    }
+}
+
+// 버튼 (입력) 이벤트
+function addElementToDisplay(element) {
+    const inputValue = inputDisplay.value;
+    const type = getElementType(element);
+
+    if (type === elementType.OPERATOR) {
+        element = processOperatorElement(inputValue, element);
+    } else if (isExistOutputDisplayValue()) {
+        return;
+    } else if (type === elementType.DECIMALPOINT) {
+        element = processDecimalPointElement(inputValue, element);
+    }
+
+    inputDisplay.value += element;
+}
+
+function getAnswerAndDisplay() {
+    let inputValue = 0 + inputDisplay.value;
+    const answer = getAnswer(inputValue);
+
+    outputDisplay.value = answer;
+}
+
+function resetDisplay() {
+    inputDisplay.value = '';
+    outputDisplay.value = '';
+}
+
+function removeElement() {
+    if (isExistOutputDisplayValue()) {
+        return;
+    }
+
+    inputDisplay.value = inputDisplay.value.slice(0, -1);
+}
+
+// 결과값 있을 때
+function isExistOutputDisplayValue() {
+    if (outputDisplay.value.length) {
+        return true;
+    }
+    return false;
+}
+
+// 연산자 처리
+function processOperatorElement(inputValue, element) {
+    if (isExistOutputDisplayValue()) {
+        inputDisplay.value = outputDisplay.value + element;
+        outputDisplay.value = '';
+        return '';
+    }
+
+    const lastElementType = getLastElementType(inputValue);
+    if (lastElementType === elementType.OPERATOR) {
+        return '';
+    }
+
+    return element;
+}
+
+// 소수점 처리
+function processDecimalPointElement(inputValue, element) {
+    const lastElementType = getLastElementType(inputValue);
+    if (lastElementType === elementType.DECIMALPOINT) {
+        return '';
+    }
+
+    if (lastElementType !== elementType.NUMBER) {
+        return '0' + element;
+    }
+
+    return element;
+}
